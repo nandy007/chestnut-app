7bc0887b252dc3c4cd445616e9d11f388d92319d
huangnan <huangnan@nj.fiberhome.com.cn>
2017-08-08 10:01:19 +0800

    完善sourcebalancer规则算法

--- proxy/sourcebalancer.js
+++ proxy/sourcebalancer.js
@@ -1,21 +1,4 @@
-const crypto = require('crypto');
-
-const encode = function (str) {
-    const cipher = crypto.createCipher('aes-128-ecb', 'roundrobin');
-    return cipher.update(str, 'utf8', 'hex') + cipher.final('hex');
-};
-
-const decode = function (str) {
-    const decipher = crypto.createDecipher('aes-128-ecb', 'roundrobin');
-    return decipher.update(str, 'hex', 'utf8') + decipher.final('utf8');
-};
-
-const getClientIp = function (req) {
-    return req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-};
+const codec = require('chestnut-utils').codec;
 
 const cookies = function (req) {
     let Cookies = {};
@@ -37,31 +20,19 @@ module.exports = function (config) {
 
         if (target) {
             // 如果有头信息则解码内部的服务器信息
-            const server = decode(target);
+            const server = codec.aesDecipher(target, 'proxy');
             // 如果此服务器在配置的服务器数组里则继续使用
-            target = options.servers.indexOf(server) ? server : null;
+            target = servers.indexOf(server) ? server : null;
         }
 
         // 如果没有分配服务器信息，大多数情况应该是指首次访问时，则分配服务器
         if (!target) {
             // 取出第一个服务器分配给当前客户端，与基础roundrobin算法一致
-            options.servers.push(target = options.servers.shift());
+            servers.push(target = servers.shift());
             // 并且将服务器信息加密后设置到cookie中
-            res.setHeader("Set-Cookie", cookieName + '=' + encode(target));
+            res.setHeader("Set-Cookie", cookieName + '=' + codec.aesCipher(target, 'proxy'));
         }
         // 通过代理指向分配的服务器
         proxy.web(req, res, { target: target });
     };
-};
-
-
-module.exports = {
-    "port": "3000",
-    "rule": function (options) {
-
-    },
-    servers: [
-        'http://127.0.0.1:3001',
-        'http://127.0.0.1:3002'
-    ]
 };
